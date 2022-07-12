0ad817a7ed351d3b74b1b205657d9fff02e95306
ppedvLukasK <lukask@ppedv.de>
2022-07-12 14:19:50 +0200

    08 Aggregatsfunktionen

---
+++ 08_Aggregatsfunkionen.sql
@@ -0,0 +1,68 @@
+USE Northwind;
+
+--COUNT(Spaltenname oder *): Gibt die Anzahl an Resultaten aus
+SELECT COUNT(*) FROM Customers;
+
+SELECT COUNT(*) FROM Customers WHERE Country = 'UK'; --7
+
+SELECT COUNT(DISTINCT City) FROM Customers; --Anzahl Cities in der Tabelle
+
+--GROUP BY: Gruppen erstellen anhand einer Spalte (hier Country-Gruppen: Argentinien Gruppe, Austria Gruppe, Brasilien Gruppe, ...)
+--Aggregatsfunktionen können auf diese Gruppen angewandt werden
+SELECT Country, COUNT(*) AS Anzahl FROM Customers GROUP BY Country;
+
+SELECT Quantity, COUNT(*) AS Anzahl FROM [Order Details] GROUP BY Quantity ORDER BY Anzahl DESC; --Anzahl Bestellungen pro Menge ausgeben
+
+SELECT 
+MAX(Freight) AS TeuersteBestellung, --MIN: kleinster Wert der Spalte
+MIN(Freight) AS BilligsteBestellung, --MAX: größter Wert der Spalte
+AVG(Freight) AS Durchschnittskosten, --AVG: Durchschnittlicher Wert der Spalte
+SUM(Freight) AS Gesamtkosten --SUM: Summe der Spalte
+FROM Orders;
+
+SELECT TOP 1 Freight FROM Orders ORDER BY Freight; --MIN
+SELECT TOP 1 Freight FROM Orders ORDER BY Freight DESC; --MAX
+
+SELECT Country, City FROM Customers GROUP BY City, Country; --Alle Stadt/Land Kombinationen ausgeben
+
+SELECT EmployeeID, COUNT(*) AS AnzBestellungen FROM Orders GROUP BY EmployeeID; --Anzahl Bestellungen pro Employee ausgeben
+
+SELECT EmployeeID, AVG(Freight) AS Durchschnittskosten FROM Orders GROUP BY EmployeeID ORDER BY Durchschnittskosten DESC; --Durchschnittskosten pro Employee ausgeben
+
+SELECT EmployeeID, COUNT(*) AS AnzBestellungen, AVG(Freight) AS Durchschnitt, SUM(Freight) AS Gesamtkosten
+FROM Orders
+GROUP BY EmployeeID
+ORDER BY AnzBestellungen; --Mehrere Aggregatsfunktionen gleichzeitig benutzen
+
+--WHERE vs GROUP BY + HAVING
+--WHERE filtert alle Daten bevor sie geholt werden
+--HAVING holt alle Daten und filtert danach
+
+--Alle Kunden die mindestens 10 Einkäufe getätigt haben
+SELECT CustomerID, COUNT(*) AS AnzBestellungen
+FROM Orders
+GROUP BY CustomerID
+HAVING COUNT(*) > 10;
+
+--Alle Kunden die mindestens 1000$ Frachtkosten verursacht haben
+SELECT CustomerID, COUNT(*) AS Anz, SUM(Freight) AS Gesamt
+FROM Orders
+GROUP BY CustomerID
+HAVING SUM(Freight) > 1000;
+
+--ORDER BY mit Aliases von Aggregatsfunktionen
+SELECT CustomerID, COUNT(*) AS Anz, SUM(Freight) AS Gesamt
+FROM Orders
+GROUP BY CustomerID
+HAVING SUM(Freight) > 1000
+ORDER BY Anz, Gesamt;
+
+--GROUP BY mit mehreren Spalten, alle Spalten die im SELECT ausgegeben werden sollen müssen im GROUP BY stehen (außer Aggregatsfunktionen)
+SELECT 
+Customers.CustomerID,
+Customers.CompanyName,
+AVG(Orders.Freight) AS AvgFreight
+FROM Orders 
+INNER JOIN Customers 
+ON Orders.CustomerID = Customers.CustomerID
+GROUP BY Customers.CustomerID, CompanyName;
