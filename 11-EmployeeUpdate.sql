bb605ec7c2d14422f1b726b10b28e3ce83964344
ppedvLukasK <lukask@ppedv.de>
2022-07-13 11:57:53 +0200

    Update 11-EmployeeUpdate.sql

--- 11-EmployeeUpdate.sql
+++ 11-EmployeeUpdate.sql
@@ -1,5 +1,9 @@
-ALTER TABLE Employees ADD Salary MONEY;
+USE Northwind;
+
+ALTER TABLE Employees ADD Salary MONEY; --Salary Spalte hinzufügen
 GO
+
+--Salaries hinzufügen
 UPDATE Employees SET Salary = 2500 WHERE EmployeeID = 1;
 UPDATE Employees SET Salary = 8000 WHERE EmployeeID = 2;
 UPDATE Employees SET Salary = 1800 WHERE EmployeeID = 3;
@@ -9,3 +13,5 @@ UPDATE Employees SET Salary = 3100 WHERE EmployeeID = 6;
 UPDATE Employees SET Salary = 2300 WHERE EmployeeID = 7;
 UPDATE Employees SET Salary = 2800 WHERE EmployeeID = 8;
 UPDATE Employees SET Salary = 3000 WHERE EmployeeID = 9;
+
+SELECT * FROM Employees; --Ergebnis anschauen
